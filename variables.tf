4eca6ce2d5899e3602e310a3e3044fdcdaf0a75b
czelabueno <c.zelabueno@gmail.com>
2021-02-27 15:02:20 -0500

    create rg only existing

--- variables.tf
+++ variables.tf
@@ -15,11 +15,6 @@ locals {
     tags = {
         provisionedBy = "https://github.com/czelabueno/infrastructure-as-code-testing"
     }
-
-    existing_rg = data.azurerm_resource_group.current.name != "" ? true : false
 }
 
 data "azurerm_subscription" "current" {}
-data "azurerm_resource_group" "current" {
-    name = upper("${var.type}_${var.stage}")
-}
